a5bb0293daf2341859f1748f0ed61f50138314f3
olchu <ochurkin@gmail.com>
2026-05-26 08:16:42 +0200

    init commit

---
+++ FlowType/FlowTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowTypeApp.swift
+//  FlowType
+//
+//  Created by olchu on 25. 5. 2026..
+//
+
+import SwiftUI
+
+@main
+struct FlowTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
